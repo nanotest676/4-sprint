6c7b46deed905f15c76dc8f1e7a0f9e1fad5ef19
Никита Кузнецов <nickbatmanll@gmail.com>
2023-02-01 15:24:39 +0300

    first commit

---
+++ hw02_community-master/yatube/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.index, name='main_page'),
+    path('group/<slug:slug>/', views.group_posts, name='group_progect'),
+]
