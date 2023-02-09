0f4168d5ce8be43029355b44e4dbdf5534df5d38
Никита Кузнецов <nickbatmanll@gmail.com>
2023-02-09 16:26:53 +0300

    final_1

--- hw02_community-master/yatube/users/urls.py
+++ hw02_community-master/yatube/users/urls.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
 from django.urls import path
 
 from . import views
@@ -9,7 +11,7 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
         'logout/',
-        LogoutView.as_view(template_name='users/logged_out.html'),
+        LogoutView.as_view(template_name='registration/logged_out.html'),
         name='logout'
     ),
     path(
@@ -17,4 +19,12 @@ urlpatterns = [
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
+    path('password-reset/', PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
